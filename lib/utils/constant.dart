ad2c359d2b83b23d80b394d9f8204ef5724e2916
Ali111q <bbbeat114@gmail.com>
2023-08-06 07:35:38 +0300

    main

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,4 +1,4 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 String webSocketUrl(LatLng pos) =>
-    'ws://192.168.0.114:6001/taxi-socket/osamah?lat=${pos.latitude}&lng=${pos.longitude}&token=4|5QTFLBNTYZ0BwDxMw1xzsD8uPH6o5caDYdJ9ojcU';
+    'ws://172.20.10.11:6001/taxi-socket/osamah?lat=${pos.latitude}&lng=${pos.longitude}&token=5|5pM4xvXeHW4VpZvXgHqK8Nt52Lh0jD3f5P8B1jGC';
